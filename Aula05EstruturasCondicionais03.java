37de1103e04aef1225bd48e1e7956c7a033c54f3
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2024-10-17 23:01:54 -0300

    Operador Ternário e  Else if
    conceitos sobre if else else if e operador ternário

---
+++ Aula05EstruturasCondicionais03.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+         double salario = 3000;
+         String mensagemDoar = "Eu vou doar  500 pro DevDojo";
+         String mensagemNaoDoar = "Ainda nao tenho condiçoes, mas vou ter!";
+         String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+        // operador Ternario
+        System.out.println(resultado);
+    }
+}
