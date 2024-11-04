0eaefe2dbb27470dac862509389574a7c38c8ab8
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2024-11-03 21:20:42 -0300

    Add files via upload

---
+++ BintroducaoMetodos/ImpressoraEstudante.java
@@ -0,0 +1,12 @@
+package javacore.BintroducaoMetodos.Test;
+
+import javacore.BintroducaoMetodos.dominio.Estudante2;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante2 estudante2){
+
+        System.out.println(estudante2.nome);
+        System.out.println(estudante2.idade);
+        System.out.println(estudante2.sexo);
+    }
+}
