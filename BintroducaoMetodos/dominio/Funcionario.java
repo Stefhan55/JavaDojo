ead81a2b0e71af664484642ba24096e3763c6d58
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2024-11-06 19:22:55 -0300

    classe funcionário para exercicio

---
+++ BintroducaoMetodos/dominio/Funcionario.java
@@ -0,0 +1,27 @@
+package javacore.BintroducaoMetodos.dominio;
+
+public class Funcionario {
+
+    public  String nome;
+    public int idade;
+    public double []salarios;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for (double salario: salarios ){
+            System.out.print(salario + "");
+        }
+
+    }
+    public void imprimeMediaSalario(){
+        double media = 0;
+        for (double salario: salarios){
+            media += salario;
+
+        }
+        media /= salarios.length;
+        System.out.println("\nMédia Salarial"+ media);
+    }
+
+}
